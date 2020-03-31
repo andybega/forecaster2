fcf5aa089959c3e8763093313d056eae498f30d2
Andreas Beger <adbeger@gmail.com>
2020-03-31 17:00:26 +0300

    study model results

--- models/analyze-tune-results.R
+++ models/analyze-tune-results.R
@@ -2,6 +2,9 @@
 #   Analyze tuning results
 #
 
+library(ggplot2)
+library(dplyr)
+
 tr <- read_csv("output/full-model/tuning-results.csv")
 
 p <- tr %>%
@@ -13,3 +16,15 @@ p <- tr %>%
   theme_minimal()
 p
 ggsave("output/figures/tune-results.png", plot = p, height = 8, width = 8)
+
+tr <- read_csv("output/tuning-results.csv")
+
+p <- tr %>%
+  pivot_longer(num.trees:min.node.size) %>%
+  ggplot(., aes(x = value, y = classif.auc)) +
+  facet_wrap(~task_id + name, scales = "free") +
+  geom_point() +
+  geom_smooth() +
+  theme_minimal()
+p
+ggsave("output/figures/tune-results-fcast-models.png", plot = p, height = 8, width = 8)
